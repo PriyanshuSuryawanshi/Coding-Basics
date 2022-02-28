55d01371227528ae7ee890b7af5fff31994f1548
Priyanshu Suryawanshi <ps3556911@gmail.com>
2022-02-28 21:29:15 +0530

    Updated a miniProject

--- Python/Miniproject/Whatsapp/send_wp_msg.py
+++ Python/Miniproject/Whatsapp/send_wp_msg.py
@@ -1,4 +1,4 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg('+91 9156149244', 'Hi~\n\tSent Using Python', 13, 26, 32)
-# pywhatkit.sendwhatmsg('+91 7588277703', 'Hi~Sent Using Python', 18, 9, 20) // Sarvesh Kadam
+pywhatkit.sendwhatmsg('+91 number', 'Hi~\n\tSent Using Python', 13, 26, 32)
+
